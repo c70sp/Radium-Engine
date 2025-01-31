56ea0bbf271ef0d58399bbecfa78aeb7cb02a04d
c70 <c70sp@proton.me>
2025-01-31 20:40:48 +0100

    Initial commit for Radium Engine

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/dev/projects/C++/OpenGL/Radium Engine/src/app.cpp" "CMakeFiles/main.dir/src/app.cpp.obj" "gcc" "CMakeFiles/main.dir/src/app.cpp.obj.d"
+  "C:/dev/projects/C++/OpenGL/Radium Engine/src/atom.cpp" "CMakeFiles/main.dir/src/atom.cpp.obj" "gcc" "CMakeFiles/main.dir/src/atom.cpp.obj.d"
+  "C:/dev/projects/C++/OpenGL/Radium Engine/src/controlRod.cpp" "CMakeFiles/main.dir/src/controlRod.cpp.obj" "gcc" "CMakeFiles/main.dir/src/controlRod.cpp.obj.d"
+  "C:/dev/projects/C++/OpenGL/Radium Engine/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.obj" "gcc" "CMakeFiles/main.dir/src/main.cpp.obj.d"
+  "C:/dev/projects/C++/OpenGL/Radium Engine/src/mesh3d.cpp" "CMakeFiles/main.dir/src/mesh3d.cpp.obj" "gcc" "CMakeFiles/main.dir/src/mesh3d.cpp.obj.d"
+  "C:/dev/projects/C++/OpenGL/Radium Engine/src/neutron.cpp" "CMakeFiles/main.dir/src/neutron.cpp.obj" "gcc" "CMakeFiles/main.dir/src/neutron.cpp.obj.d"
+  "C:/dev/projects/C++/OpenGL/Radium Engine/src/reactor.cpp" "CMakeFiles/main.dir/src/reactor.cpp.obj" "gcc" "CMakeFiles/main.dir/src/reactor.cpp.obj.d"
+  "C:/dev/projects/C++/OpenGL/Radium Engine/src/shader.cpp" "CMakeFiles/main.dir/src/shader.cpp.obj" "gcc" "CMakeFiles/main.dir/src/shader.cpp.obj.d"
+  "C:/dev/projects/C++/OpenGL/Radium Engine/src/utils.cpp" "CMakeFiles/main.dir/src/utils.cpp.obj" "gcc" "CMakeFiles/main.dir/src/utils.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
